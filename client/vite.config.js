a3b87a364579ecf750a69f8c693f62a0a44b8078
SubhankarA8415 <123244705+SubhankarA8415@users.noreply.github.com>
2025-07-14 18:05:38 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // 👈 this is CRUCIAL for correct asset loading
-  plugins: [react(), tailwindcss()],
+  base: '/',
+  plugins: [react(), tailwindcss()]
 })
